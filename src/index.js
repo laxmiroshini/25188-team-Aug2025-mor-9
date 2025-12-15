78033632543f1ab1cd934dca494ab6a5ff79b29a
laxmiroshini <laxmiroshini@gmail.com>
2025-12-15 09:20:12 +0530

    parameterized routing-product page

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter } from "react-router-dom";
+import MainRoute from './Router/MainRoute';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <h4>FSD-RJS SARAVANAN BATCH</h4>
-    <ol>
-      <li>Laxmi Roshini</li>
-      <li>Pavithra</li>
-      <li>Jesintha</li>
-      <li>Shenbagam</li>
-      <li>Prem Kumar</li>
-      <li>Marimuthu</li>
-      <li>Shreepal</li>
-    </ol>
+    <BrowserRouter>
+      <MainRoute />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
